391e8241f0bd7635e860c4649071a6ced9fb25c1
Charles Mailly <charles.mailly@free.fr>
2016-06-27 17:13:56 +0200

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # Reflection
-Simple to use, provide XML and JSON support
+============
+
+The goal of this library is to be easy to use and support XML and JSON with the same interface.
+
+Features :
+----------
+- XML and JSON systems
+- Simple interface
+- Serialize complex objects
+- Serialize std::vector<T> and std::map<K,T>
+
+Usage :
+-------
+
+```c++
+class A : public ReflectionObject
+{
+	public:
+		A()
+		{
+			// Init
+		}
+		
+		void serialize(ReflectionSystem& system, std::string const& identifier)
+		{
+			system.create(identifier);
+			system.save("name", m_name);
+			system.save("values", m_values);
+			system.saveComplex("test", m_test);
+			system.saveComplex("others", m_others);
+			system.end();
+		}
+		
+		bool deserialize(ReflectionSystem& system, std::string const& identifier)
+		{
+			if (system.read(identifier))
+			{
+				system.load("name", m_name); // You can add safety check for each functions
+				system.load("values", m_values);
+				system.loadComplex("test", m_test);
+				system.loadComplex("others", m_others);
+				return true;
+			}
+			return false;
+		}
+	
+	private:
+		std::string m_name;
+		std::vector<int> m_values;
+		B m_test; // B also inherit from ReflectionObject
+		std::vector<B> m_others;
+}
+
+int main()
+{
+	JSONSystem json;
+	json.openDocument("test.json"); // openDocument(filename, bool erase = false) -> true = new file
+	
+	A object;
+	a.serialize(json, "A");
+	
+	json.saveDocument(); // saveDocument(bool readable = true) -> setting it to false can be useful for networking
+	
+	A test;
+	test.deserialize(json, "A");
+	
+	// test == a
+	
+	return 0;
+}
+```
+
+Problems :
+----------
+The support of both file format add few restrictions about the serialization. I had to find a way to make it "cross-file"
+Because of XML : Array and Map can't be parsed with the JSON standard.
+Because of JSON : You can't have few objects at the same node level with the same name.
+
+Dependencies :
+----------
+XML : https://github.com/zeux/pugixml
+JSON : https://github.com/nlohmann/json
---
+++ src/Reflection.hpp
@@ -0,0 +1,254 @@
+#ifndef REFLECTIONSYSTEM_HPP
+#define REFLECTIONSYSTEM_HPP
+
+#include <map>
+#include <sstream>
+#include <vector>
+#include <string>
+
+class ReflectionSystem;
+
+class ReflectionObject
+{
+public:
+	ReflectionObject() {};
+	~ReflectionObject() {};
+
+	virtual void serialize(ReflectionSystem& system, std::string const& identifier) const {};
+	virtual bool deserialize(ReflectionSystem& system, std::string const& identifier) { return false; };
+};
+
+class ReflectionSystem
+{
+public:
+	ReflectionSystem() {};
+	~ReflectionSystem() {};
+
+	// Write
+	virtual void create(std::string const& identifier) {};
+	virtual void save(std::string const& identifier, std::string const& value) {};
+	virtual void save(std::string const& identifier, int const& value) { save(identifier, toString(value)); };
+	virtual void save(std::string const& identifier, float const& value) { save(identifier, toString(value)); };
+	virtual void save(std::string const& identifier, char const& value) { save(identifier, toString(value)); };
+	virtual void save(std::string const& identifier, double const& value) { save(identifier, toString(value)); };
+	virtual void save(std::string const& identifier, unsigned int const& value) { save(identifier, toString(value)); };
+	virtual void save(std::string const& identifier, bool const& value) { save(identifier, toString(value)); };
+	template <typename T>
+	void save(std::string const& identifier, std::vector<T> const& vector)
+	{
+	    create(identifier);
+        save("size", vector.size());
+        for (std::size_t i = 0; i < vector.size(); ++i)
+        {
+            save(toString(i), vector.at(i));
+        }
+        end();
+	}
+	template <typename T>
+	void saveComplex(std::string const& identifier, std::vector<T> const& vector)
+	{
+        create(identifier);
+        save("size", vector.size());
+        for (std::size_t i = 0; i < vector.size(); ++i)
+        {
+            vector.at(i).serialize(*this, toString(i));
+        }
+        end();
+	}
+	template <typename K, typename T>
+	void save(std::string const& identifier, std::map<K,T> const& map)
+	{
+	    create(identifier);
+        save("size", map.size());
+        std::size_t i = 0;
+        for (auto itr = map.begin(); itr != map.end(); itr++)
+        {
+            save(toString(i), std::string(toString(itr->first) + "||" + toString(itr->second)));
+            i++;
+        }
+        end();
+	}
+	template <typename K, typename T>
+	void saveComplex(std::string const& identifier, std::map<K,T> const& map)
+	{
+        create(identifier);
+        save("size", map.size());
+        std::size_t i = 0;
+        for (auto itr = map.begin(); itr != map.end(); itr++)
+        {
+            std::string key = toString(itr->first);
+            save(toString(i), key);
+            itr->second.serialize(*this, key);
+            i++;
+        }
+        end();
+	}
+	template <typename T>
+	void saveComplex(std::string const& identifier, T const& t)
+	{
+	    t.serialize(*this,identifier);
+	}
+	virtual void end() {};
+
+    // Read
+    virtual bool read(std::string const& identifier) { return false; };
+	virtual bool load(std::string const& identifier, std::string& value) { return false; };
+	virtual bool load(std::string const& identifier, int& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<int>(temp); return true; } return false; };
+	virtual bool load(std::string const& identifier, float& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<float>(temp); return true; } return false; };
+	virtual bool load(std::string const& identifier, char& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<char>(temp); return true; } return false; };
+	virtual bool load(std::string const& identifier, double& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<double>(temp); return true; } return false; };
+	virtual bool load(std::string const& identifier, unsigned int& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<unsigned int>(temp); return true; } return false; };
+	virtual bool load(std::string const& identifier, bool& value) { std::string temp = ""; if (load(identifier, temp)) { value = fromString<bool>(temp); return true; } return false; };
+	virtual std::string load(std::string const& identifier) { std::string temp = ""; load(identifier, temp); return temp; };
+	template <typename T>
+	bool load(std::string const& identifier, std::vector<T>& vector)
+	{
+	    bool ret = true;
+        if (read(identifier))
+        {
+            std::size_t size;
+            if (load("size", size))
+            {
+                for (std::size_t i = 0; i < size; ++i)
+                {
+                    T value;
+                    if (!load(toString(i),value))
+                    {
+                        value = T();
+                        ret = false;
+                    }
+                    vector.push_back(value);
+                }
+                end();
+                return ret;
+            }
+            end();
+        }
+        return false;
+	}
+	template <typename T>
+	bool loadComplex(std::string const& identifier, std::vector<T>& vector)
+	{
+	    bool ret = true;
+        if (read(identifier))
+        {
+            std::size_t size;
+            if (load("size", size))
+            {
+                for (std::size_t i = 0; i < size; ++i)
+                {
+                    vector.push_back(T());
+                    if (!loadComplex(toString(i), vector.back()))
+                    {
+                        ret = false;
+                    }
+                }
+                end();
+                return ret;
+            }
+            end();
+        }
+        return false;
+	}
+	template <typename K, typename T>
+	bool load(std::string const& identifier, std::map<K,T>& map)
+	{
+	    bool ret = true;
+        if (read(identifier))
+        {
+            std::size_t size;
+            if (load("size", size))
+            {
+                for (std::size_t i = 0; i < size; ++i)
+                {
+                    std::string temp;
+                    if (load(toString(i), temp))
+                    {
+                        std::size_t f = temp.find_first_of("||");
+                        K k;
+                        T t;
+                        if (f != std::string::npos)
+                        {
+                            k = fromString<K>(temp.substr(0,f));
+                            t = fromString<T>(temp.substr(f+2));
+                        }
+                        else
+                        {
+                            ret = false;
+                        }
+                        map.emplace(k, t);
+                    }
+                    else
+                    {
+                        end();
+                        return false;
+                    }
+                }
+                end();
+                return ret;
+            }
+            end();
+        }
+        return false;
+	}
+	template <typename K, typename T>
+	bool loadComplex(std::string const& identifier, std::map<K,T>& map)
+	{
+	    bool ret = true;
+        if (read(identifier));
+        {
+            std::size_t size;
+            if (load("size", size))
+            {
+                for (std::size_t i = 0; i < size; ++i)
+                {
+                    std::string key;
+                    if (load(toString(i), key))
+                    {
+                        T t;
+                        if (!loadComplex(key, t))
+                        {
+                            ret = false;
+                            t = T();
+                        }
+                        map.emplace(fromString<K>(key), t);
+                    }
+                    else
+                    {
+                        end();
+                        return false;
+                    }
+                }
+                end();
+                return ret;
+            }
+            end();
+        }
+        return false;
+	}
+	template <typename T>
+	bool loadComplex(std::string const& identifier, T& t)
+	{
+        return t.deserialize(*this, identifier);
+	}
+
+protected:
+    template <typename T>
+    std::string toString(const T& value)
+    {
+        std::ostringstream os;
+        os << value;
+        return os.str();
+    }
+
+    template <typename T>
+    T fromString(const std::string& str)
+    {
+        std::istringstream i(str);
+        T t;
+        i >> t;
+        return t;
+    }
+};
+
+#endif // REFLECTIONSYSTEM_HPP
